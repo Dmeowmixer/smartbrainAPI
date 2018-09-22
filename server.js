25f1cbef6d3eb55e847b733d9bfe26b3bb219c54
Dmeowmixer <ransonikuo@gmail.com>
2018-09-22 12:25:54 -1000

    fixes server.js hopefully please work I beg of you

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res)=>{ res.send('it is working') })
 
-app.post('/signin', (req,res) => {signin.handleSignin(db,bcrypt)})
+app.post('/signin', signin.handleSignin(db,bcrypt))
 
 app.post('/register',(req,res) => { register.handleRegister(req,res,db,bcrypt) })
 
